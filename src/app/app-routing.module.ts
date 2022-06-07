0c213e7c02ac8274e8ce47ed462902896ea23109
Ortega Collado <david.ortega-collado@capgemini.com>
2022-06-07 17:48:16 +0200

    modificaciones grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimatedbackgroundComponent } from './components/animatedbackground/animatedbackground.component';
+import { FlexComponent } from './components/flex/flex.component';
 import { GridComponent } from './components/grid/grid.component';
 import { HomeComponent } from './components/home/home.component';
 import { UIComponent } from './components/ui/ui.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path:"UI", component : UIComponent },
   { path:"animatedbackground", component : AnimatedbackgroundComponent },
   { path:"grid", component : GridComponent },
+  { path:"flex", component : FlexComponent },
   //animatedbackground
   { path: "", redirectTo:"/", pathMatch : "full" },
   { path: "**", redirectTo: "/", pathMatch : "full" }
